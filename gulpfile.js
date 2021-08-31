c7daa874374a34a41ed93eb0348c9f447c47714d
phoxett <phoxett@gmail.com>
2021-08-31 16:48:41 +0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -4,16 +4,15 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const del = require('del')
 const nodemon = require('gulp-nodemon')
-const livereload = require('gulp-livereload')
 const path = require('path')
 
-gulp.task('build-sass', () =>
+gulp.task('sass', () =>
 {
-    return gulp.src(path.join(__dirname, '/src/src/sass/*.scss'))
+
+    gulp.src(path.join(__dirname, '/src/src/sass/*.scss'))
         .pipe(sass()
             .on('error', sass.logError))
         .pipe(gulp.dest('src/src/css'))
-        .pipe(livereload())
 })
 
 gulp.task('del-css', () =>
@@ -23,36 +22,38 @@ gulp.task('del-css', () =>
     ])
 })
 
-gulp.task('watch-sass', () =>
+gulp.task('watch', () =>
 {
-    livereload.listen()
-    gulp.watch(['src', 'src/src', 'src/src/data', 'src/src/sass', 'src/src/css', 'src/src/hbs', 'src/src/hbs/parts'], gulp.series(['del-css', 'build-sass', 'start']))
+    gulp.watch([
+        'src',
+        'src/src',
+        'src/src/data',
+        'src/src/sass',
+        'src/src/css',
+        'src/src/hbs',
+        'src/src/hbs/parts'
+    ], gulp.series(['del-css', 'sass']))
+
 })
 
-gulp.task('start', function(done)
+
+gulp.task('nodemon', function(done)
 {
     nodemon(
-    {
-        script: 'src/app.js',
-        ext: 'js html sass css json',
-        env:
         {
-            'NODE_ENV': 'development'
-        },
-        watch: ['src', 'src/src', 'src/src/sass', 'src/src/css', 'src/src/hbs', 'src/src/hbs/parts', './node_modules/uikit/dist/js', './node_modules/uikit/dist/css'],
-        done: done,
-        stdout: true
-    }).on('change', () =>
-    {
-        del([
-            'src/src/sass/*.css'
-        ])
-        return gulp.src(path.join('/src/src/sass/*.scss'))
-        .pipe(sass()
-            .on('error', sass.logError))
-        .pipe(gulp.dest('src/src/css/'))
-        .pipe(livereload())
-    })
+            script: 'src/app.js',
+            ext: 'js html sass css json',
+            env:
+            {
+                'NODE_ENV': 'development'
+            },
+            task: ['watch'],
+            watch: ['src', 'src/src', 'src/src/sass', 'src/src/css', 'src/src/hbs', 'src/src/hbs/parts', './node_modules/uikit/dist/js', './node_modules/uikit/dist/css'],
+            done: done,
+            stdout: true
+        })
+        .on('start', ['watch'])
+
 })
 
-gulp.task('default', gulp.series(['del-css', 'watch-sass', 'start']))
+gulp.task('default', gulp.series(['nodemon', 'del-css', 'watch', ]))
