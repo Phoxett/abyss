3969a132b5430cb812bb9d473e92aa973e5f76b9
phoxett <phoxett@gmail.com>
2021-09-01 10:52:14 +0200

    test

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,37 @@
-
 const gulp = require('gulp')
 const nodemon = require('gulp-nodemon')
 
-gulp.task('start', function (done) {
-    nodemon({
-      script: 'src/app.js'
-    , ext: 'js html'
-    , env: { 'NODE_ENV': 'development' }
-    , done: done,
-    watch: [
+gulp.task('start', function(done)
+{
+    nodemon(
+        {
+            script: 'src/app.js',
+            ext: 'js html',
+            env:
+            {
+                'NODE_ENV': 'development'
+            },
+            done: done,
+            watch: [
+                'src', 'src/src',
+                'src/src/hbs', 'src/src/hbs/parts'
+            ]
+        })
+        .on('restart', function()
+        {
+            setTimeout(() =>
+            {
+                const date = new Date()
+                console.log(`restarted   [${date.getHours().toString()}:${date.getMinutes().toString()}:${date.getMilliseconds().toString()}]`)
+            }, 1000)
+            console.log('restarting')
+        })
+})
+
+gulp.task('default', gulp.series['start'], () =>
+{
+    gulp.watch([
         'src', 'src/src',
         'src/src/hbs', 'src/src/hbs/parts'
-    ]
-    })
-  })
-
-
-gulp.task('default', gulp.series['start'])
+    ], (event) => nodemon.emit('restart'))
+})
